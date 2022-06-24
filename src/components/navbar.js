7f8f723111d8b01efbe1a170a508b7799cc7ccb2
Alexandru1099 <cervinschi_alex@yahoo.com>
2022-06-24 10:22:54 +0300

    wip create references

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./components.css";
-import { useState } from "react";
 
-const Navbar = (placeholder) => {
-  const [searchTerm, setSerchTerm] = useState("");
-  return (
-    <div className="navbar w-100">
-      <span className="text-white m-4 fs-3">Welcome to RVmDB</span>
-      <input
-        className="search text-center mx-5"
-        type="text"
-        placeholder={placeholder}
-        name="search"
-        onChange={(event) => setSerchTerm(event.target.value)}
-      />
-    </div>
-  );
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  searchMovie(movie)
+  {
+    console.log(movie);
+    this.props.refMovie = movie;
+  }
+  render() {
+    return (
+      <div className="navbar w-100">
+        <span className="text-white m-4 fs-3">Welcome to RVmDB</span>
+        <input className="search text-center mx-5" type="text" placeholder="Search for a movie..." name="search" onChange={(e)=>{this.props.setFunction(e.target.value);}} />
+      </div>
+    )
+  };
 };
 export default Navbar;
--- src/home/home.js
+++ src/home/home.js
@@ -1,12 +1,33 @@
+import React from "react";
 import Navbar from "../components/navbar";
 import ListMovie from "../components/ListMovie";
+import { LocalLaundryService } from "@mui/icons-material";
 
-const Home = () => {
-  return (
-    <div className="home">
-      <Navbar />
-      <ListMovie />
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      movies: [],
+      refMovie: React.createRef()
+    };
+    console.log(this.state);
+    console.log(props)
+
+  }
+  setMovie(a) {
+    console.log(a);
+    // Setstate({ refMovie: a});
+    // this.refMovie = a;
+  }
+  render() {
+    return (
+      <div className="home">
+        <Navbar refMovie={this.refMovie} setFunction={this.setMovie} />
+        <ListMovie refMovie={this.refMovie} />
+      </div>
+    );
+  };
+}
 export default Home;
