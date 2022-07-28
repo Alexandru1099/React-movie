8f6dd718796e580ac0a64e65eb44814da3b231b3
Alexandru1099 <95041772+Alexandru1099@users.noreply.github.com>
2022-07-28 18:54:25 +0300

    add auth in pages

--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../components.css";
 import "./selectedMovie.css";
+import { useSelector } from "react-redux/es/exports";
+import { Redirect } from "react-router-dom";
 import DetailsMovieSelected from "../DetailsMovieSelected";
 import SimilarMovies from "../Recomandation/SimilarMovies";
 
 const SelectMovie = () => {
+  const isAuth = useSelector((state) => state.authentication.authentication);
+  console.log(isAuth);
   
   return (
     <section className="mx-5">
--- src/pages/RecomandationPage.js
+++ src/pages/RecomandationPage.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Recomandation from "../components/Recomandation/Recomandation";
 import SimilarMovies from "../components/Recomandation/SimilarMovies";
+import { useSelector } from "react-redux/es/exports";
+import { Redirect } from "react-router-dom";
+
 
 const RecomandationPage = () => {
+
+    const isAuth = useSelector((state) => state.authentication.authentication);
+    console.log(isAuth);
     return (
         <section>
-        <Recomandation />
-       </section>
-       )
+            {isAuth ? (
+                <Recomandation />
+            ) : (
+                <Redirect to="/signup" />
+            )}
+        </section>
+    )
 }
 export default RecomandationPage;
